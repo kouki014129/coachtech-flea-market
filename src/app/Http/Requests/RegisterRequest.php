db7c66a26b72e10b464992bca00daa78ec7456f9
yamada koki <kouki.014129@icloud.com>
2026-05-26 23:32:14 +0900

    Add explicit password_confirmation validation and tweak register layout spacing

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -26,6 +26,7 @@ class RegisterRequest extends FormRequest
             'name' => ['required', 'string', 'max:20'],
             'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users', 'email')],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password_confirmation' => ['required', 'string', 'min:8', 'same:password'],
         ];
     }
 
@@ -43,6 +44,9 @@ class RegisterRequest extends FormRequest
             'password.required' => 'パスワードを入力してください',
             'password.min' => 'パスワードは8文字以上で入力してください',
             'password.confirmed' => 'パスワードと一致しません',
+            'password_confirmation.required' => 'パスワードと一致しません',
+            'password_confirmation.min' => 'パスワードと一致しません',
+            'password_confirmation.same' => 'パスワードと一致しません',
         ];
     }
 }
